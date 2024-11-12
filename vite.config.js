33c7073770f09639c61b60c111efc5509c57431e
Yiftach Samocha <yiftachsamo@gmail.com>
2024-11-12 16:56:03 +0200

    Add React

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
 	build: {
-	  outDir: 'dist', // Ensure the build output is in the dist folder
-	}
-  }
+		outDir: 'dist',
+		emptyOutDir: true,
+	},
+})
