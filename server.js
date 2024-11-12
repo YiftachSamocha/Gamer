d6a5f6d2dd8b357702f8163e5c1d1d3e46b553a4
Yiftach Samocha <yiftachsamo@gmail.com>
2024-11-12 16:34:34 +0200

    Add meta

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current directory path from import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +15,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Serve the index.html file for all other routes
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-})
+});
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
